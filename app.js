7f99d9e6850c7960bab82ea416e0cf07b2fb6267
Dominic Fantauzzo <fantauzd@oregonstate.edu>
2024-05-22 13:01:33 -0700

    modify app.js to use handlebars

--- app.js
+++ app.js
@@ -3,7 +3,8 @@
 /*
     SETUP
 */
-var db = require('./database/db-connector') // Database
+const { engine } = require('express-handlebars');
+var db = require('./database/db-connector'); // Database
 var express = require('express');           // We are using the express library for the web server
 var app     = express();                    // We need to instantiate an express object to interact with the server in our code
 PORT        = 9861;                         // Set a port number at the top so it's easy to change in the future
@@ -13,33 +14,8 @@ PORT        = 9861;                         // Set a port number at the top so i
 */
 app.get('/', function(req, res)
     {
-        // Define our queries
-        query1 = 'DROP TABLE IF EXISTS diagnostic;';
-        query2 = 'CREATE TABLE diagnostic(id INT PRIMARY KEY AUTO_INCREMENT, text VARCHAR(255) NOT NULL);';
-        query3 = 'INSERT INTO diagnostic (text) VALUES ("MySQL is working for fantauzd!")'; //replace with your onid
-        query4 = 'SELECT * FROM diagnostic;';
-
-        // Execute every query in an asynchronous manner, we want each query to finish before the next one starts
-
-        // DROP TABLE...
-        db.pool.query(query1, function (err, results, fields){
-
-            // CREATE TABLE...
-            db.pool.query(query2, function(err, results, fields){
-
-                // INSERT INTO...
-                db.pool.query(query3, function(err, results, fields){
-
-                    // SELECT *...
-                    db.pool.query(query4, function(err, results, fields){
-
-                        // Send the results to the browser
-                        res.send(JSON.stringify(results));
-                    });
-                });
-            });
-        });
-    });                                       // requesting the web site.
+        res.render('index');                    // Note the call to render() and not send(). Using render() ensures the templating engine
+    });                                         // will process this file, before sending the finished HTML to the client.
 
 /*
     LISTENER
