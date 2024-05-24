69befa3706b6ede7cc0f39ad9a66dee22f4c52c9
Dominic Fantauzzo <fantauzd@oregonstate.edu>
2024-05-23 19:08:36 -0700

    added date value attribute to table

--- public/js/delete_breed.js
+++ public/js/delete_breed.js
@@ -20,7 +20,19 @@ function deleteRow(breedID){
     for (let i = 0, row; row = table.rows[i]; i++) {
        if (table.rows[i].getAttribute("data-value") == breedID) {
             table.deleteRow(i);
+            deleteDropDownMenu(breedID);
             break;
        }
     }
+}
+
+function deleteDropDownMenu(breedID){
+  let selectMenu = document.getElementById("select-name");
+  for (let i = 0; i < selectMenu.length; i++){
+    if (Number(selectMenu.options[i].value) === Number(breedID)){
+      selectMenu[i].remove();
+      break;
+    } 
+
+  }
 }
